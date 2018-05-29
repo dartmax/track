72c3469afa9a1d6c5f99c9e458fba975e676a4db
Nicholas <kolia.starinskiy@gmail.com>
2018-05-29 17:38:07 +0300

    Update new.js

--- new.js
+++ new.js
@@ -1,36 +1,18 @@
 const editMap = (arr) => {
 
-  const states = [];
-  const carriers = [];
-  const next_tracking_ids = [];
-  const origin = {};
-  const status = {};
-  const weight = {};
-  const destination = {};
+return arr.reduce(function(prev, curr, index, arr) {
+  const states = [...prev.tracks.get(), ...curr.tracks.get()];
+  const carriers = [prev.name, curr.name];
+  const next_tracking_ids = [prev.next_tracking_ids, curr.next_tracking_ids];
 
-  arr.forEach((i, el) => {
-    states.push(...i.tracks.get());
-    carriers.push(i.name);
-    next_tracking_ids.push(i.next_tracking_ids);
-    origin[el] = i.origin,
-    status[el] = i.status,
-    weight[el] = i.weight,
-    destination[el] = i.destination
-  });
-
-  if (!states.length) {
-    return {
-      error: "NO_DATA"
-    }
-  } else {
-    return {
-      states,
-      carriers,
-      next_tracking_ids,
-      origin: origin[0],
-      destination: destination[0],
-      status: status[0],
-      weight: weight[0]
-    };
-  }
+  return {
+    states,
+    carriers,
+    next_tracking_ids,
+    origin: curr.origin,
+    destination: curr.destination,
+    weight: curr.weight,
+    status: curr.status
+  };
+});
 }
